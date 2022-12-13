9410b483012e236a91cce7a72d8cebe30d154bfb
Matheus Ferreira da Silva <matheus@Matheuss-MacBook-Air.local>
2022-12-12 23:24:22 -0300

    FEAT: setup nest and prisma

---
+++ src/prisma.service.ts
@@ -0,0 +1,16 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async enableShutdownHooks(app: INestApplication) {
+        this.$on('beforeExit', async () => {
+            // close the app if the database connection is closed
+            await app.close();
+        });
+    }
+}
